adb37c39f9fdd0441e8e5e0a7912eca862b265fe
Ryan Wick <ryan.wick@oregonstate.edu>
2015-11-10 13:24:17 -0800

    Added cache file for subjects. Moved lcsh_search out to separate method.

--- mapping_methods/subject.rb
+++ mapping_methods/subject.rb
@@ -3,31 +3,76 @@ require 'linkeddata'
 module Qa; end
 require 'qa/authorities/web_service_base'
 require 'qa/authorities/loc'
+
 module MappingMethods
   module Subject
-    def lcsubject(subject, data)
+
+    def subject_cache
+      unless @subject_cache
+        if File.exist?("cache_subject.yml")
+          @subject_cache = YAML.load(File.read("cache_subject.yml"))
+          @log.info "Loading #{@subject_cache.length} entries from Subject Cache"
+        end
+      end
+      @subject_cache ||= {}
+    end
+
+    def lcsh_search(str)
       authority = Qa::Authorities::Loc.new
+
+      begin
+        uri = authority.search("#{str}", "subjects").find{|x| x["label"].strip.downcase == str.downcase}
+      rescue StandardError => e
+        puts e
+      end
+
+      uri ||= ""
+
+      @log.debug("LCSH Result URI: " + uri.to_s)
+
+      if !uri.nil? && uri != ""
+        parsed_uri = uri["id"].gsub("info:lc", "http://id.loc.gov")
+        @log.info("Subject Result found for " + str + ": " + parsed_uri)
+        subject_cache[str] = {:uri => RDF::URI(parsed_uri), :label => str}
+      else
+        @log.warn "No Subject found for #{str}" unless subject_cache.include?(str.downcase)
+        subject_cache[str] = {:uri => str, :label => str}
+      end
+
+      File.open("cache_subject.yml", 'w') do |f|
+        f.write subject_cache.to_yaml
+      end
+
+      subject_cache
+    end
+
+
+    def lcsubject(subject, data)
       graph = RDF::Graph.new
-      @lcsubject_cache ||= {}
+
       Array(data.split(';')).each do |subject_name|
         subject_name.strip!
         subject_name.gsub!('"', "")
         next if subject_name.gsub("-","") == ""
-        begin
-          uri = @lcsubject_cache[subject_name.downcase] || authority.search("#{subject_name}", "subjects").find{|x| x["label"].strip.downcase == subject_name.downcase}
-        rescue StandardError => e
-          puts e
+
+        @log.debug("Subject split: " + subject_name)
+
+        unless subject_cache.include? subject_name
+          begin
+            lcsh_search(subject_name)
+          rescue => e
+            puts subject, subject_name, e.backtrace
+          end
         end
-        uri ||= ""
-        if !uri.nil? && uri != "" 
-          parsed_uri = uri["id"].gsub("info:lc", "http://id.loc.gov")
-          graph << RDF::Statement.new(subject, RDF::Vocab::DC.subject, RDF::URI(parsed_uri))
+
+        if subject_cache[subject_name][:uri].to_s.include?('http')
+          graph << RDF::Statement.new(subject, RDF::Vocab::DC.subject, subject_cache[subject_name][:uri])
         else
-          puts "No subject heading found for #{subject_name}" unless @lcsubject_cache.include?(subject_name.downcase)
+          @log.warn("Subject URI not found: " + subject_name)
           graph << RDF::Statement.new(subject, RDF::Vocab::DC11.subject, subject_name)
         end
-        @lcsubject_cache[subject_name.downcase] ||= uri
       end
+
       graph
     end
 
