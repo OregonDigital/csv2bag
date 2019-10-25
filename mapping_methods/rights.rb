36fcf509b7f60763432cfda8c04611811b7892b3
Ryan Wick <ryan.wick@oregonstate.edu>
2019-10-25 15:51:45 -0700

    Create License method, move CC there.

--- mapping_methods/rights.rb
+++ mapping_methods/rights.rb
@@ -2,24 +2,8 @@ require 'rdf'
 
 module MappingMethods
   module Rights
-    ODLICENSES = 
+    ODRIGHTS =
       [
-        { :title => "Public Domain Mark",
-          :uri => RDF::URI('http://creativecommons.org/publicdomain/mark/1.0/') },
-        { :title => "Creative Commons CC0 Universal Public Domain",
-          :uri => RDF::URI('http://creativecommons.org/publicdomain/zero/1.0/') }, 
-        { :title => "Creative Commons - Attribution (BY)",
-          :uri => RDF::URI('http://creativecommons.org/licenses/by/4.0/') },
-        { :title => "Creative Commons - Attribution, ShareAlike (BY-SA)",
-          :uri => RDF::URI('http://creativecommons.org/licenses/by-sa/4.0/') },
-        { :title => "Creative Commons - Attribution, No Derivatives (BY-ND)",
-          :uri => RDF::URI(' http://creativecommons.org/licenses/by-nd/4.0/') },
-        { :title => "Creative Commons - Attribution, Non-Commercial (BY-NC)",
-          :uri => RDF::URI('http://creativecommons.org/licenses/by-nc/4.0/') },
-        { :title => "Creative Commons - Attribution, Non-Commercial, ShareAlike (BY-NC-SA)",
-          :uri => RDF::URI('http://creativecommons.org/licenses/by-nc-sa/4.0/') },
-        { :title => "Creative Commons - Attribution, Non-Commercial, No Derivatives (BY-NC-ND)",
-          :uri => RDF::URI('http://creativecommons.org/licenses/by-nc-nd/4.0/') },
         { :title => "Educational Use Permitted",
           :uri => RDF::URI('http://opaquenamespace.org/ns/rights/educational') },
         { :title => "Rights Reserved - Free Access",
@@ -29,7 +13,32 @@ module MappingMethods
         { :title => "Orphan Works",
           :uri => RDF::URI('http://opaquenamespace.org/ns/rights/orphan-work-us') },
         { :title => "Unknown",
-          :uri => RDF::URI('http://opaquenamespace.org/ns/rights/unknown') }
+          :uri => RDF::URI('http://opaquenamespace.org/ns/rights/unknown') },
+
+        { :label => 'COPYRIGHT NOT EVALUATED',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/CNE/1.0/') },
+        { :label => 'IN COPYRIGHT - EDUCATIONAL USE PERMITTED',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/InC-EDU/1.0/') },
+        { :label => 'IN COPYRIGHT - NON-COMMERCIAL USE PERMITTED',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/InC-NC/1.0/') },
+        { :label => 'IN COPYRIGHT - EU ORPHAN WORK',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/InC-OW-EU/1.0/') },
+        { :label => 'IN COPYRIGHT - RIGHTS-HOLDER(S) UNLOCATABLE OR UNIDENTIFIABLE',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/InC-RUU/1.0/') },
+        { :label => 'IN COPYRIGHT',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/InC/1.0/') },
+        { :label => 'NO KNOWN COPYRIGHT',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/NKC/1.0/') },
+        { :label => 'NO COPYRIGHT - CONTRACTUAL RESTRICTIONS',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/NoC-CR/1.0/') },
+        { :label => 'NO COPYRIGHT - NON-COMMERCIAL USE ONLY',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/NoC-NC/1.0/') },
+        { :label => 'NO COPYRIGHT - OTHER KNOWN LEGAL RESTRICTIONS',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/NoC-OKLR/1.0/') },
+        { :label => 'NO COPYRIGHT - UNITED STATES',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/NoC-US/1.0/') },
+        { :label => 'COPYRIGHT UNDETERMINED',
+          :uri => RDF::URI('http://rightsstatements.org/vocab/UND/1.0/') }
       ]
 
     def rights(subject, data)
@@ -37,19 +46,19 @@ module MappingMethods
       rightsURI = ""
 
       if data.include?('http')
-        # Check if URI matches allowed license URIs
-        ODLICENSES.each do |odlicense|
-          rightsURI << odlicense[:uri] if RDF::URI(data) == odlicense[:uri]
+        # Check if URI matches allowed rights URIs
+        ODRIGHTS.each do |odrights|
+          rightsURI << odrights[:uri] if RDF::URI(data) == odrights[:uri]
         end
 
-        if rightsURI.to_s.empty? then @log.error("Rights URI does not match any acceptable licenses: " + data) end
+        if rightsURI.to_s.empty? then @log.error("Rights URI does not match any acceptable value: " + data) end
       else
         # Check if text matches allowed license title
-        ODLICENSES.each do |odlicense|
-          rightsURI << odlicense[:uri] if data == odlicense[:title]
+        ODRIGHTS.each do |odrights|
+          rightsURI << odrights[:uri] if data == odrights[:title]
         end
 
-        if rightsURI.to_s.empty? then @log.error("Rights text does not match any acceptable licenses: " + data) end
+        if rightsURI.to_s.empty? then @log.error("Rights text does not match any acceptable value: " + data) end
       end
 
       if !rightsURI.to_s.empty?
